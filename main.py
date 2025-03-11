3cf911f8b22b23f7c566f58692fd182f59b5b872
Robert Joseph Saltos <Rsaltos@csustan.edu>
2025-03-10 22:19:25 -0700

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ import numpy as np
 siteName = "Turlock CA USA"
 SampleRate = "1h"
 st.header = "Turlock AOD"
-StartDate = st.date_input("StartDate", datetime.date(2024, 1, 1))
+StartDate = st.date_input("StartDate", datetime.date(2024, 5, 1))
 StartDateTime = datetime.datetime.combine(StartDate, datetime.time(0, 0))
-EndDate = st.date_input("EndDate", datetime.date(2024, 1, 7))
+EndDate = st.date_input("EndDate", datetime.date(2024, 5, 8))
 EndDateTime = datetime.datetime.combine(EndDate, datetime.time(23, 59))
 
 # Allow the user to set y-axis limits
